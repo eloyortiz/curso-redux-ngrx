42db7c8aa61215c4c2ba27140672fae97c5fab40
Eloy Ortiz <ortizpulido.eloy@gmail.com>
2023-01-31 13:37:57 +0100

    feat: add ngrx/store-devtools

--- 02-redux-app/src/app/app.component.ts
+++ 02-redux-app/src/app/app.component.ts
@@ -15,10 +15,9 @@ export class AppComponent {
   contador!: number;
 
   constructor(private store: Store<AppState>) {
-    this.store.subscribe((state) => {
-      console.log(state);
-      this.contador = state.contador;
-    });
+    this.store
+      .select('contador')
+      .subscribe((contador) => (this.contador = contador));
   }
 
   incrementar() {
--- 02-redux-app/src/app/app.module.ts
+++ 02-redux-app/src/app/app.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { contadorReducer } from './contador/contador.reducer';
 
 import { AppComponent } from './app.component';
@@ -10,7 +12,17 @@ import { NietoComponent } from './contador/nieto/nieto.component';
 
 @NgModule({
   declarations: [AppComponent, HijoComponent, NietoComponent],
-  imports: [BrowserModule, StoreModule.forRoot({ contador: contadorReducer })],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({ contador: contadorReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      // autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      // trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      // traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
