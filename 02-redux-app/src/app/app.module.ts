2a0d193982b8cb4883996a2e55c7668479a0e546
Eloy Ortiz <ortizpulido.eloy@gmail.com>
2023-02-03 09:40:21 +0100

    fix: replace isDevMode from angular/core for environment.production

--- 02-redux-app/src/app/app.module.ts
+++ 02-redux-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { contadorReducer } from './contador/contador.reducer';
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HijoComponent, NietoComponent],
@@ -17,7 +18,7 @@ import { NietoComponent } from './contador/nieto/nieto.component';
     StoreModule.forRoot({ contador: contadorReducer }),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
-      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      logOnly: environment.production, // Restrict extension to log-only mode
       // autoPause: true, // Pauses recording actions and state changes when the extension window is not open
       // trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
       // traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
--- 03-todoapp/src/app/app.module.ts
+++ 03-todoapp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { isDevMode, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { ReactiveFormsModule } from '@angular/forms';
@@ -7,6 +7,7 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { appReducers } from './app.reducer';
 
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { TodoModule } from './todos/todo.module';
@@ -25,7 +26,7 @@ import { TodoModule } from './todos/todo.module';
     }),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
-      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      logOnly: environment.production, // Restrict extension to log-only mode
       // autoPause: true, // Pauses recording actions and state changes when the extension window is not open
       // trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
       // traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
