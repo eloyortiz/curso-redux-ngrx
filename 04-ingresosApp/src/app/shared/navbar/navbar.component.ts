35aefc9a5dd9c1a86add68a4136d248f0fc2ea9f
Eloy Ortiz <ortizpulido.eloy@gmail.com>
2023-02-07 17:10:58 +0100

    feat: set user name in navbar from Store

--- 04-ingresosApp/src/app/shared/navbar/navbar.component.ts
+++ 04-ingresosApp/src/app/shared/navbar/navbar.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { User } from 'src/app/models/user.model';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styles: [
-  ]
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styles: [],
 })
 export class NavbarComponent implements OnInit {
+	user!: User | any;
 
-  constructor() { }
+	constructor(private store: Store<AppState>) {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.store.select('user').subscribe(({ user }) => (this.user = user));
+	}
 }
